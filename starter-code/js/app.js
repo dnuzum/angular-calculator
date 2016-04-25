087b24f7a62acf2f01491d22f3596632aeeee978
dnuzum <derek.nuzum@gmail.com>
2016-04-25 11:05:43 -0700

    need to fix commits

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,1 +1,23 @@
-console.log('app.js was loaded.');
+// console.log('app.js was loaded.');
+var app = angular.module("CalcApp", []);
+
+var calcController = app.controller("CalcCtrl", [
+  "$scope",
+  function($scope) {
+    $scope.calcValue = 0;
+    $scope.memory = null;
+    $scope.result = 0;
+    $scope.operation = null;
+
+    $scope.numClick = function(num) {
+      if ($scope.calcValue == 0) {
+        $scope.calcValue = num;
+      } else {
+        $scope.calcValue += String(num);
+      }
+      $scope.calcValue = Number($scope.calcValue)
+    };
+
+
+  }
+]);
